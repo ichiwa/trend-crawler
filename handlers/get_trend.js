1168861a46727af07baa5c5adbf68ccd02c82485
hayataso <hayataso@devalon.biz>
2015-10-07 17:51:31 +0900

    first commit

---
+++ handlers/get_trend.js
@@ -0,0 +1,7 @@
+/**
+ * trendの取得
+ */
+module.exports = function(request, reply){
+  var config = require('config');
+  reply('Hello, world!');
+}
---
+++ index.js
@@ -0,0 +1,52 @@
+var Hapi = require('hapi');
+var Good = require('good');
+var Hoek = require('hoek');
+var config = require('config');
+
+var server = new Hapi.Server();
+server.connection({ port: config.get("port") });
+
+server.register(require('vision'), function (err) {
+    Hoek.assert(!err, err);
+    server.views({
+        engines: {
+            html: require('handlebars')
+        },
+        relativeTo: __dirname,
+        path: 'templates'
+    });
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+      reply.view('index');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/get_trend',
+  handler: require('./handlers/get_trend')
+})
+
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: {
+                response: '*',
+                log: '*'
+            }
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
