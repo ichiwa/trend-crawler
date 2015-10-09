84e548bec3a5c5ef6c0063a523e61f30066a9589
Ichiwa <shimokawa@devalon.biz>
2015-10-09 22:00:30 +0900

    finished

--- batch/getTrendsExe.js
+++ batch/getTrendsExe.js
@@ -1,9 +1,7 @@
-module.exports = function(){
-  require('./../lib/get_trend_core')(function(err){
-    if (err){
-      console.error(err);
-    } else {
-      console.log("success");
-    }
-  })  
-};
+require('./../lib/get_trend_core')(function(err){
+  if (err){
+    console.error(err);
+  } else {
+    console.log("success");
+  }
+})
